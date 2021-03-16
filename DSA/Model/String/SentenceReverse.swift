23877fedefb42def53c5d50285d68cf3b7a02f31
Ethan <ethan.halprin@gmail.com>
2021-03-16 13:16:18 +0200

    Reverse Sentence

---
+++ DSA/Model/String/SentenceReverse.swift
@@ -0,0 +1,54 @@
+//
+//  SentenceReverse.swift
+//  DSA
+//
+//  Created by Ethan on 10/03/2021.
+//
+
+import Foundation
+
+/*
+    Let a string be a sentnece, reverse all words e.g.
+ 
+      input:  "I learn swift now"
+      output: "now swift learn I"
+ */
+extension Str {
+    
+    func reverseSentence(_ text: [Character]) -> [Character] {
+        
+        guard !isSingleWorded(text) else {
+            return text
+        }
+        
+        let first = extractHead(text)
+        let rev   = first + text
+        
+        return rev
+    }
+    
+    func isSingleWorded(_ text: [Character]) -> Bool {
+        
+        var result = true
+        
+        for ch in text {
+            if ch == " " {
+                result = false
+                break
+            }
+        }
+        return result
+    }
+    
+    func extractHead(_ text: [Character]) -> [Character] {
+        
+        var i = 0
+        var result = [Character]()
+        
+        while text[i] != " " {
+            result.append(text[i])
+            i += 1
+        }
+        return result
+    }
+}
